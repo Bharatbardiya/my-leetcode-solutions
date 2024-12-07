ba6eeaae13c158621fc1348a6d0517deee709646
Bharat Bardiya <75123794+Bharatbardiya@users.noreply.github.com>
2024-12-07 12:44:35 +0530

    Time: 0 ms (100%), Space: 19.6 MB (46.91%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,23 +1,66 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>&adj, int s, vector<bool>&vis){
-        if(vis[s]) return;
-        vis[s] = true;
-        for(int i = 0; i<adj.size(); i++){
-            if(i!=s and adj[s][i]==1){
-                dfs(adj, i, vis);
+    // void dfs(vector<vector<int>>&adj, int s, vector<bool>&vis){
+    //     if(vis[s]) return;
+    //     vis[s] = true;
+    //     for(int i = 0; i<adj.size(); i++){
+    //         if(i!=s and adj[s][i]==1){
+    //             dfs(adj, i, vis);
+    //         }
+    //     }
+    // }
+    class DisjointSet{
+        vector<int>rank, parent;
+        public:
+        DisjointSet(int n){
+            for(int i = 0; i<n; i++){
+                parent.push_back(i);
+            }
+            rank.resize(n);
+        }
+
+        void _union(int x, int y){
+            int pr_x = find(x), pr_y = find(y);
+            if(pr_x==pr_y) return;
+
+            if(rank[pr_x]>rank[pr_y]) parent[pr_y] = pr_x;
+            else if(rank[pr_x]<rank[pr_y]) parent[pr_x] = pr_y;
+            else {
+                rank[pr_x]++;
+                parent[pr_y] = pr_x;
             }
         }
-    }
+        
+        int find(int x){
+            if(parent[x]==x) return x;
+            return parent[x] = find(parent[x]);
+        }
+
+    };
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
-        vector<bool>vis(n,false);
-        int ans = 0;
+        // vector<bool>vis(n,false);
+        // int ans = 0;
+        // for(int i = 0; i<n; i++){
+        //     if(vis[i]) continue;
+        //     dfs(isConnected, i, vis);
+        //     ans++;
+        // }
+        // return ans;
+        
+        DisjointSet myst(n);
+
         for(int i = 0; i<n; i++){
-            if(vis[i]) continue;
-            dfs(isConnected, i, vis);
-            ans++;
+            for(int j = 0; j<i; j++){
+                if(isConnected[i][j]==1){
+                    myst._union(i,j);
+                }
+            }
         }
-        return ans;
+        unordered_set<int>st;
+        for(int i = 0; i<n; i++){
+            st.insert(myst.find(i));
+        }
+        return st.size();
     }
 };
