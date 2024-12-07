60f53f2151f869618ca446deafaa2fc83df8409c
Bharat Bardiya <75123794+Bharatbardiya@users.noreply.github.com>
2024-12-08 01:30:14 +0530

    Time: 6 ms (88.82%), Space: 51.3 MB (91.47%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
+    bool dfs(vector<vector<int>>&graph, vector<bool>&vis, vector<bool>&pathvis, int s){
+        if(pathvis[s]) return false;
+        if(vis[s]) return true;
+
+        vis[s] = true;
+        pathvis[s] = true;
+
+        bool flag = true;
+        for(auto x : graph[s]){
+            flag = flag and dfs(graph, vis, pathvis, x);
+        }
+        if(flag) pathvis[s] = false;
+        return flag;
+    }
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<vector<int>>adj(n);
-        vector<int>og(n);
-        queue<int>q;
-        
+        vector<bool>vis(n,false), pathvis(n,false);
+
         for(int i = 0; i<n; i++){
-            for(auto x : graph[i]){
-                adj[x].push_back(i);
-            }
-            og[i] = graph[i].size();
-            if (og[i]==0) q.push(i);
-        }
-        
-        while(q.size()>0){
-            int i = 0;
-            int u = q.front();
-            q.pop();
-            for(auto x : adj[u]){
-                og[x]--;
-                if(og[x]==0){
-                    q.push(x);
-                }
+            if(vis[i]==false){
+                dfs(graph, vis, pathvis, i);
             }
         }
         vector<int>ans;
         for(int i = 0; i<n; i++){
-            if(og[i]==0) ans.push_back(i);
+            if(pathvis[i]==false) ans.push_back(i);
         }
         return ans;
     }
