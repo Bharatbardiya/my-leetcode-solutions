53ef324cd80a64b549c732e8530a34724310d202
Bharat Bardiya <75123794+Bharatbardiya@users.noreply.github.com>
2024-12-15 00:36:17 +0530

    Time: 2 ms (17.07%), Space: 9.3 MB (9.55%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -8,8 +8,13 @@ public:
 			return dp[i][j] = fun(i-1,j, dp) + fun(i,j-1, dp);
 		}
     int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m, vector<int>(n,-1));
+        vector<vector<int>>dp(m, vector<int>(n,1));
 
-        return fun(m-1, n-1, dp);
+        for(int i = 1; i<m; i++){
+            for(int j = 1; j<n; j++){
+                dp[i][j] = dp[i-1][j]+dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
