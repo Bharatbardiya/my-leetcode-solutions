2c61c0326080ee30ae9396f66c8e4e608476a6e5
Bharat Bardiya <75123794+Bharatbardiya@users.noreply.github.com>
2024-12-17 13:04:52 +0530

    Time: 55 ms (22.9%), Space: 52.9 MB (10.06%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -15,8 +15,23 @@ public:
 
     int coinChange(vector<int>& coins, int amount) {
         int n = coins.size();
-        vector<vector<int>>dp(n+1, vector<int>(amount+1,-1));
-        int val=fun(coins, n-1, amount, dp);
-        return val>=1e8?-1:val;
+        vector<vector<int>>dp(n+1, vector<int>(amount+1,0));
+        // int val=fun(coins, n-1, amount, dp);
+        // return val>=1e8?-1:val;
+        for(int j = 1; j<=amount; j++){
+            dp[0][j] = 1e8;
+        }
+
+        for(int i = 1; i<=n; i++){
+            
+            for(int j = 0; j<=amount; j++){
+                int pick = INT_MAX, notpick = dp[i-1][j];
+                if(coins[i-1]<=j){
+                    pick = 1+dp[i][j-coins[i-1]];
+                }
+                dp[i][j] = min(pick,notpick);
+            }
+        }
+        return dp[n][amount]>=1e8?-1:dp[n][amount];
     }
 };
