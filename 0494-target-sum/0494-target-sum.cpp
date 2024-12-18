edb6c1ece2fb3167b752711ccaeae81711c30b60
Bharat Bardiya <75123794+Bharatbardiya@users.noreply.github.com>
2024-12-18 16:38:30 +0530

    Time: 19 ms (54.67%), Space: 28.3 MB (25.04%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
-    int fun(vector<int>&nums, int target, int i, map<pair<int,int>,int>&dp){
+    int fun(vector<int>&nums, int target, int i, vector<vector<int>>&dp, int limit){
         if(i<0){
             return target==0;
         }
-        if(dp.find({i,target})!=dp.end()) return dp[{i,target}];
+        // cout<<target<<","<<limit<<", "<<dp[0].size()<<endl;
+        if(dp[i][target+limit]!=-1) return dp[i][target+limit];
 
-        int plus = fun(nums, target+nums[i], i-1, dp);
-        int minus = fun(nums, target-nums[i], i-1, dp);
-        return dp[{i,target}] = plus+minus;
+        int plus = fun(nums, target+nums[i], i-1, dp, limit);
+        int minus = fun(nums, target-nums[i], i-1, dp, limit);
+        return dp[i][target+limit] = plus+minus;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
         int sum = 0;
         for(auto x : nums) sum+=x;
-        map<pair<int,int>, int>dp;
-        return fun(nums, target, nums.size()-1, dp);
+        sum = 2*sum+2*abs(target);
+        int limit = sum/2;
+        vector<vector<int>>dp(nums.size()+1, vector<int>(sum+2,-1));
+        return fun(nums, target, nums.size()-1, dp, limit);
     }
 };
