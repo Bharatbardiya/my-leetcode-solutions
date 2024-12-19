1a8d4f888eb12754339ff8e652512489702d85b1
Bharat Bardiya <75123794+Bharatbardiya@users.noreply.github.com>
2024-12-19 23:16:33 +0530

    Time: 0 ms (100%), Space: 9.2 MB (35.22%) - LeetHub

--- 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
+++ 0780-max-chunks-to-make-sorted/0780-max-chunks-to-make-sorted.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int maxChunksToSorted(vector<int>& arr) {
-        int sum = 0;
-        int real_sum = 0, ans = 0;
+        int sum = 0, ans = 0;
         for(int i = 0; i<arr.size(); i++){
             sum+=arr[i];
-            real_sum+=i;
-            if(sum==real_sum) ans++;
+            sum-=i;
+            if(!sum) ans++;
         }
         return ans;
     }
