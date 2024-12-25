81be9eea1d54787452dfb32892d680362134e4ac
Bharat Bardiya <75123794+Bharatbardiya@users.noreply.github.com>
2024-12-25 18:26:47 +0530

    Time: 20 ms (34.22%), Space: 11.6 MB (23.46%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -8,33 +8,32 @@ public:
     }
 
     string minWindow(string s, string t) {
-        vector<int>fq1(100), fq2(100);
+        unordered_map<char, int>feqt, window;
+
         int n = s.length();
         int i = 0, j = 0;
         
-        for(auto c : t) fq1[c-'A']++;
+        for(auto c : t) feqt[c]++;
         int size = 100001, ind = -1;
+        int need = feqt.size(), have = 0;
 
         while(j<n){
-            if(isvalid(fq1,fq2)){
-                if(j-i<size){
-                    size = j-i;
-                    ind = i;
+            window[s[j]]++;
+            if(feqt.count(s[j]) and feqt[s[j]]==window[s[j]]){
+                have++;
+            }
+            while(need==have){
+                if((j-i+1)<size){
+                    // cout<<i<<","<<j<<endl;
+                    size = j-i+1; ind = i;
                 }
-                fq2[s[i]-'A']--;
+                window[s[i]]--;
+                if(feqt.count(s[i]) and feqt[s[i]] > window[s[i]]){
+                    have--;
+                }
                 i++;
             }
-            else{
-                fq2[s[j]-'A']++;
-                j++;
-            }
-        }
-        while(isvalid(fq1,fq2)){
-            if(j-i<size){
-                size=j-i;
-                ind = i;
-            }
-            fq2[s[i]-'A']--; i++;
+            j++;
         }
         if(ind==-1) return "";
         return s.substr(ind, size);
