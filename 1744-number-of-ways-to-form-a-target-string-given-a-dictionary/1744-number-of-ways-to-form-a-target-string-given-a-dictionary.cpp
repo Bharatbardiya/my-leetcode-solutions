00e219d3e71c12714f00dfb2334531743f1c6301
Bharat Bardiya <75123794+Bharatbardiya@users.noreply.github.com>
2024-12-29 23:49:53 +0530

    Time: 74 ms (68.37%), Space: 83.3 MB (26.28%) - LeetHub

--- 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/1744-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
+++ 1744-number-of-ways-to-form-a-target-string-given-a-dictionary/1744-number-of-ways-to-form-a-target-string-given-a-dictionary.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
-    long long fun(vector<vector<int>>&mywords, int i, int n, string &target, int j, vector<vector<int>>&dp){
-        if(j==target.length()) return 1;
-        if(i==n) return 0;
+    long long fun(vector<vector<int>>&mywords, int i, string &target, int j, vector<vector<int>>&dp){
+        if(j<0) return 1;
+        if(i<0) return 0;
 
         if(dp[i][j]!=-1) return dp[i][j];
         int count = mywords[i][target[j]-'a'];
-        long long ans = (count)*fun(mywords, i+1, n, target, j+1, dp);
-        ans += fun(mywords, i+1, n, target, j, dp);
+        long long ans = (count)*fun(mywords, i-1, target, j-1, dp);
+        ans += fun(mywords, i-1, target, j, dp);
         int mod = 1e9+7;
         return dp[i][j] = (ans)%(mod);
     }
@@ -20,8 +20,16 @@ public:
             }
         }
 
-        vector<vector<int>>dp(n+1, vector<int>(m+1, -1));
+        vector<vector<long long>>dp(n+1, vector<long long>(m+1, 0));
+        int mod = 1e9+7;
 
-        return fun(mywords, 0, n, target, 0, dp);
+        for(int i = 0; i<=n; i++) dp[i][0]=1;
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=m;  j++){
+                int count = mywords[i-1][target[j-1]-'a'];
+                dp[i][j] = (count*dp[i-1][j-1] + dp[i-1][j])%mod;
+            }
+        }
+        return dp[n][m];
     }
 };
