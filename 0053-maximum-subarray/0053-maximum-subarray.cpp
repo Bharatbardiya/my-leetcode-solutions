ec1a47bd7c970e54015b610977640b6b0a8b32f9
Bharat Bardiya <75123794+Bharatbardiya@users.noreply.github.com>
2024-12-31 12:51:42 +0530

    Time: 2777 ms (5.19%), Space: 71.7 MB (32.92%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int ans = INT_MIN, curr = 0;
-        for(int i = 0; i<nums.size(); i++){
-            curr+=nums[i];
-            ans = max(curr, ans);
-            if(curr<0) curr = 0;
+    int divideAndConquer(vector<int>&nums, int l, int r){
+        cout<<l<<","<<r<<"),";
+        if(l>r) return -1000000;
+        else if(l==r) return nums[l];
+
+        int mid = (l+r)/2;
+        int left = divideAndConquer(nums, l, mid-1);
+        int right = divideAndConquer(nums, mid+1, r);
+        
+        int ans = nums[mid];
+        int leftMaxi = 0, rightMaxi = 0, i = mid-1, curr = 0;
+        while(i>=l){
+            curr+=nums[i]; i--;
+            leftMaxi = max(curr, leftMaxi);
         }
-        return ans;
+        i = mid+1, curr = 0;
+        while(i<=r){
+            curr+=nums[i]; i++;
+            rightMaxi = max(curr, rightMaxi);
+        }
+        return max(ans+leftMaxi+rightMaxi, max(left, right));
+    }
+    int maxSubArray(vector<int>& nums) {
+        
+        return divideAndConquer(nums, 0, nums.size()-1);
     }
 };
