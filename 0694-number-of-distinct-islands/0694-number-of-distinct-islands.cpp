fd107162e15e442489ad9315f70c4b6d7288487c
Bharat Bardiya <75123794+Bharatbardiya@users.noreply.github.com>
2025-01-04 20:20:04 +0530

    Time: 11 ms (75.07%), Space: 34.9 MB (63.44%) - LeetHub

--- 0694-number-of-distinct-islands/0694-number-of-distinct-islands.cpp
+++ 0694-number-of-distinct-islands/0694-number-of-distinct-islands.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>&grid, int i, int j, vector<pair<int,int>>&points, vector<vector<bool>>&vis){
+    void dfs(vector<vector<int>>&grid, int i, int j, string &path, vector<vector<bool>>&vis, char dir){
         int n = grid.size(), m = grid[0].size();
         if(i>=n or j>=m or i<0 or j<0) return;
         if(vis[i][j]) return;
         if(grid[i][j]==0) return;
 
         vis[i][j]=true;
-        points.push_back({i,j});
-        dfs(grid, i-1, j, points, vis);
-        dfs(grid, i+1, j, points, vis);
-        dfs(grid, i, j-1, points, vis);
-        dfs(grid, i, j+1, points, vis);
+        path.push_back(dir);
+        dfs(grid, i, j+1, path, vis, 'R');
+        dfs(grid, i+1, j, path, vis, 'D');
+        dfs(grid, i, j-1, path, vis, 'L');
+        dfs(grid, i-1, j, path, vis, 'U');
+        path.push_back('0');
     }
     void transform(vector<pair<int,int>>&points, int a, int b){
         for(auto &pr: points){
@@ -21,16 +22,14 @@ public:
     int numDistinctIslands(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
         vector<vector<bool>>vis(n, vector<bool>(m,false));
-        set<vector<pair<int,int>>>st;
+        unordered_set<string>st;
 
         for(int i = 0; i<n; i++){
             for(int j = 0; j<m; j++){
                 if(vis[i][j]==false and grid[i][j]==1){
-                    vector<pair<int,int>>points;
-                    dfs(grid, i,j, points, vis);
-                    transform(points, i,j);
-                    // sort(points.begin(), points.end());
-                    st.insert(points);
+                    string path = "";
+                    dfs(grid, i,j, path, vis, 'R');
+                    st.insert(path);
                 }
             }
         }
