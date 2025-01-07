5f6fa94388b8024cdd38e310e1ece88c3c84998c
Bharat Bardiya <75123794+Bharatbardiya@users.noreply.github.com>
2025-01-07 15:26:32 +0530

    Time: 0 ms (100%), Space: 7.6 MB (89.14%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,20 +1,25 @@
 class Solution {
 public:
+    int calculateValue(int x){
+        int ans = 0;
+        while(x>0){
+            int rem = x%10;
+            ans += (rem*rem);
+            x = x/10;
+        }
+        return ans;
+    }
     bool isHappy(int n) {
         //2, 4, 16, 37, 58, 25+64=98, 81+64=145, 1+16+25=42, 20, 4,
-        unordered_set<int>st;
-        while(n>1){
-            int ans = 0;
-            while(n>0){
-                int rem = n%10;
-                ans += (rem*rem);
-                n = n/10;
-            }
-            if(ans==1) break;
-            if(st.count(ans)) return false;
-            st.insert(ans);
-            n = ans;
-        }
+        int t1 = n, t2 = n;
+        do{
+            t1 = calculateValue(t1);
+            t2 = calculateValue(calculateValue(t2));
+            if(t1==1 or t2==1) break;
+            else if(t1==t2) return false;
+            
+        }while(true);
+
         return true;
     }
 };
