3bab005b8df7e12f7b1084f83d99f2e5e6446ef9
Bharat Bardiya <75123794+Bharatbardiya@users.noreply.github.com>
2025-01-08 21:19:28 +0530

    Time: 4 ms (35.34%), Space: 9.5 MB (41.87%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.cpp
+++ 0010-regular-expression-matching/0010-regular-expression-matching.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    bool explore(string &s, vector<pair<char,char>>&vpr, int i, int j){
+    bool explore(string &s, vector<pair<char,char>>&vpr, int i, int j, vector<vector<int>>&dp){
         int n = s.length(), m = vpr.size();
         if(i>=n){
             if(j==m) return true;
-            else if(vpr[j].second=='*') return explore(s,vpr,i,j+1);
+            else if(vpr[j].second=='*') return explore(s,vpr,i,j+1, dp);
             return false;
         }
         if(j>=m) return false;
 
+        if(dp[i][j]!=-1) return dp[i][j];
         if(vpr[j].second=='*'){
-            if(explore(s, vpr, i, j+1)) return true;
-            if(vpr[j].first=='.') return explore(s, vpr, i+1, j);
-            else if(vpr[j].first==s[i]) return explore(s, vpr, i+1, j);
-            else return false;
+            if(explore(s, vpr, i, j+1, dp)) return dp[i][j] = true;
+            if(vpr[j].first=='.') return dp[i][j] = explore(s, vpr, i+1, j, dp);
+            else if(vpr[j].first==s[i]) return dp[i][j] = explore(s, vpr, i+1, j, dp);
+            else return dp[i][j] = false;
         }
-        if(vpr[j].first=='.') return explore(s, vpr, i+1, j+1);
-        else if(vpr[j].first==s[i]) return explore(s, vpr, i+1, j+1);
-        return false;
+        if(vpr[j].first=='.') return dp[i][j] = explore(s, vpr, i+1, j+1, dp);
+        else if(vpr[j].first==s[i]) return dp[i][j] = explore(s, vpr, i+1, j+1, dp);
+        return dp[i][j] = false;
     }
 
     vector<pair<char,char>>convertPattern(string &s){
@@ -37,6 +38,8 @@ public:
     }
     bool isMatch(string s, string p) {
         vector<pair<char,char>>vpr = convertPattern(p);
-        return explore(s,vpr, 0,0);
+        int n = s.length(), m = vpr.size();
+        vector<vector<int>>dp(n+1, vector<int>(m+1, -1));
+        return explore(s,vpr, 0,0, dp);
     }
 };
