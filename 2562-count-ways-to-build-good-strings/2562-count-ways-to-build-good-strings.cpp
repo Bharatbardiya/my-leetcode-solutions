24914e402c1bdfef90b432ee657adfceb30539f4
Bharat Bardiya <75123794+Bharatbardiya@users.noreply.github.com>
2025-01-12 22:17:47 +0530

    Time: 10 ms (25.07%), Space: 19.7 MB (24.77%) - LeetHub

--- 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
+++ 2562-count-ways-to-build-good-strings/2562-count-ways-to-build-good-strings.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
     int mod = 1e9+7;
-    int reqfun(int val, int low, int high, int zero, int one, vector<int>&dp){
-        if(val>high) return 0;
+    int fun(int val, int x1, int x2, vector<long long>&dp){
+        if(val<0) return 0;
+        else if(val==0) return 1;
         if(dp[val]!=-1) return dp[val];
 
-        long long ans = 0;
-        if(val>=low and val<=high) ans = 1;
-        ans += reqfun(val+zero, low, high, zero, one, dp);
-        ans += reqfun(val+one, low, high, zero, one, dp);
-        return dp[val] = (ans)%mod;
+        return dp[val] = (fun(val-x1, x1, x2, dp)+fun(val-x2, x1,x2, dp))%mod;
     }
     int countGoodStrings(int low, int high, int zero, int one) {
-        vector<int>dp(high+1, -1);
-        return reqfun(0, low, high, zero, one, dp);
+        vector<long long>dp(high+1, -1);
+        int ans = 0;
+        for(int i = low; i<=high; i++){
+            ans += fun(i, zero, one, dp);
+            // ans += dp[i];
+            ans = (ans%mod);
+        }
+        return ans;
     }
 };
