2479aa13e0aff0b5bf11313251a7b5057d3a9e74
Bharat Bardiya <75123794+Bharatbardiya@users.noreply.github.com>
2025-01-22 00:55:37 +0530

    Time: 19 ms (84.45%), Space: 15.5 MB (76.31%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.cpp
@@ -1,5 +1,11 @@
 class Solution {
 public:
+    int gcd(int x, int y){
+        if(x<y) return gcd(y,x);
+        if(y==0) return x;
+        return gcd(y, x%y);
+    }
+
     int maxPoints(vector<vector<int>>& points) {
         int n = points.size();
         int ans = min(2, (int)points.size());
@@ -7,15 +13,26 @@ public:
         // => (y1-y2)*(x2-x3) = (y2-y3)*(x1-x2)
         for(int i = 0; i<n; i++){
             int x1 = points[i][0], y1 = points[i][1];
+            // int temp = 0;
+            map<pair<int,int>,int>mp;
             for(int j = i+1; j<n; j++){
-                int temp = 2;
                 int x2 = points[j][0], y2 = points[j][1];
-                for(int k = j+1; k<n; k++){
-                    int x3 = points[k][0], y3 = points[k][1];
-
-                    if((y1-y2)*(x2-x3) == (y2-y3)*(x1-x2)) temp++;
+                int top = (y1-y2), bottom = (x1-x2);
+                if(bottom<0){
+                    top *= -1;
+                    bottom *= -1;
                 }
-                ans = max(ans, temp);
+                if(top==0){
+                    bottom = 0;
+                }
+                else if(bottom==0){
+                    top=INT_MAX; bottom=INT_MAX;
+                }
+                int my_gcd = gcd(abs(top), abs(bottom));
+                if(my_gcd>0 and top!=INT_MAX) top /= my_gcd;
+                if(my_gcd>0 and top!=INT_MAX) bottom /= my_gcd;
+                mp[{top, bottom}]++;
+                ans = max(ans, mp[{top, bottom}]+1);
             }
 
         }
