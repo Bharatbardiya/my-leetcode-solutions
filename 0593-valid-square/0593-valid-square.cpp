c3ea5dad152905b6be234cc85cef232b7b058c38
Bharat Bardiya <75123794+Bharatbardiya@users.noreply.github.com>
2025-01-22 11:01:52 +0530

    Time: 0 ms (100%), Space: 29.8 MB (44.25%) - LeetHub

--- 0593-valid-square/0593-valid-square.cpp
+++ 0593-valid-square/0593-valid-square.cpp
@@ -4,53 +4,21 @@ public:
         return pow((p1[0]-p2[0]), 2) + pow((p1[1]-p2[1]), 2);
     }
     bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
-        
-        int d2 = squareDist(p1,p2), d3 = squareDist(p1,p3), d4 = squareDist(p1, p4);
-        // cout<<d2<<","<<d3<<","<<d4<<endl;
-        if(d2!=d3 and d3!=d4 and d4!=d2) return false;
-        else if(d2==d3 and d3==d4) return false;
-        // int digonal = -1;
-        vector<vector<int>>dig1, dig2;
-        dig1.push_back(p1);
-        if(d2==d4){
-            // cout<<"yes";
-            if(d3<=d2) return false;
-            dig1.push_back(p3);
-            dig2.push_back(p2);
-            dig2.push_back(p4);
-        }
-        else if(d3==d4){
-            
-            if(d2<=d4) return false;
-            dig1.push_back(p2);
-            dig2.push_back(p3);
-            dig2.push_back(p4);
-        }
-        else{
-            if(d4<=d2) return false;
-            dig1.push_back(p4);
-            dig2.push_back(p2);
-            dig2.push_back(p3);
-        }
-
-        // for(auto p : dig1){
-        //     cout<<p[0]<<","<<p[1]<<"),";
-        // }
-        // cout<<endl;
-        // for(auto p : dig2){
-        //     cout<<p[0]<<","<<p[1]<<"),";
-        // }
-        // cout<<endl;
-
-
-        if(squareDist(dig1[0], dig1[1])!=squareDist(dig2[0], dig2[1])) 
+        vector<vector<int>>points = {p1, p2, p3, p4};
+        sort(points.begin(), points.end());
+        int dig1 = squareDist(points[0], points[3]);
+        int dig2 = squareDist(points[1], points[2]);
+        if(dig1!=dig2){
             return false;
-        int e1 = squareDist(dig1[0], dig2[0]);
-        int e2 = squareDist(dig1[0], dig2[1]);
-        int e3 = squareDist(dig1[1], dig2[0]);
-        int e4 = squareDist(dig1[1], dig2[1]);
-
-        if(e1!=e2 or e2!=e3 or e3!=e4) return false;
-        return true;
+        }
+        int d1 = squareDist(points[0], points[1]);
+        int d2 = squareDist(points[1], points[3]);
+        int d3 = squareDist(points[3], points[2]);
+        int d4 = squareDist(points[2], points[0]);
+        // cout<<dig1<<","<<dig2<<endl;
+        // cout<<d1<<","<<d2<<","<<d3<<","<<d4<<endl;
+        if(d1==0 or d2 == 0 or d3 == 0 or d4 == 0) return false;
+        if(d1==d2 and d2==d3 and d3==d4 and d4==d1) return true;
+        return false;
     }
 };
