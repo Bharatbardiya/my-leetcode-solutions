5232b7925316377393b85f0f2d7e9746228855b9
Bharat Bardiya <75123794+Bharatbardiya@users.noreply.github.com>
2025-02-12 14:21:49 +0530

    Time: 122 ms (7.5%), Space: 104.9 MB (5.1%) - LeetHub

--- 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
+++ 2473-max-sum-of-a-pair-with-equal-sum-of-digits/2473-max-sum-of-a-pair-with-equal-sum-of-digits.cpp
@@ -9,19 +9,19 @@ public:
         return ans;
     }
     int maximumSum(vector<int>& nums) {
-        unordered_map<int, multiset<int, greater<int>> >mp;
+        unordered_map<int, multiset<int> >mp;
         int Ans = -1;
         for(auto x : nums){
             int digit_sum = digitSum(x);
             mp[digit_sum].insert(x);
+            if(mp[digit_sum].size()>2)
+                mp[digit_sum].erase(mp[digit_sum].begin());
         }
         for(auto pr : mp){
             // cout<<pr.first<<","<<pr.second.size()<<"),";
-            if(pr.second.size()>=2){
-                auto itr = pr.second.begin();
-                int temp = *itr;
-                itr++;
-                temp += *itr;
+            if(pr.second.size()==2){
+                int temp = 0;
+                for(auto t : pr.second) temp+=t;
                 Ans = max(Ans, temp);
             }
         }
