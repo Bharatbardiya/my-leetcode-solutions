789b3b15ab37d771594ac3c8284968ba6459140f
Bharat Bardiya <75123794+Bharatbardiya@users.noreply.github.com>
2025-04-10 00:22:20 +0530

    Time: 0 ms (100%), Space: 17 MB (92.36%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -1,20 +1,22 @@
 class Solution {
 public:
-    bool dfs(vector<vector<int>>&graph, vector<char>&color, int s, char prev){
+    bool dfs(vector<vector<int>>&graph, vector<char>&color, int s){
         
-        if(color[s]!='#'){
-            return prev!=color[s];
-        }
-        if(color[s]=='#'){
-            if(prev=='a') color[s] = 'b';
-            else color[s] = 'a';
-        }
         for(auto u : graph[s]){
-            bool f = dfs(graph, color, u, color[s]);
-            if(!f) return false;
+            if(color[u]=='#'){
+                if(color[s]=='w'){
+                    color[u] = 'b';
+                }
+                else color[u] = 'w';
+                bool f = dfs(graph, color, u);
+                if (!f) return false;
+            }
+            if(color[u]==color[s]) return false;
         }
+
         return true;
     }
+
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
 
@@ -22,7 +24,8 @@ public:
 
         for(int i = 0; i<n; i++){
             if(color[i]=='#'){
-                bool f = dfs(graph, color, i, 'a');
+                color[i] = 'w';
+                bool f = dfs(graph, color, i);
                 if(!f) return false;
             }
         }
