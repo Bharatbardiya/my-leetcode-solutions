7ce2dd87a538d6fe1dde8e98e6c1197303fdd788
Bharat Bardiya <75123794+Bharatbardiya@users.noreply.github.com>
2025-04-11 10:59:29 +0530

    Time: 3 ms (72.88%), Space: 14.9 MB (36.07%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int,int>mp;
+        for(int i = 0; i<nums.size(); i++){
+            int x = nums[i];
+            if(mp.find(target-x)!=mp.end()){
+                return {mp[target-x], i};
+            }
+            mp[x] = i;
+        }
+        return {-1,-1};
+    }
+};
