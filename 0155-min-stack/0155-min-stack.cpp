77f45d30b4895b4b2f7aaf545772b73141d05204
Bharat Bardiya <75123794+Bharatbardiya@users.noreply.github.com>
2025-04-14 17:51:53 +0530

    Time: 0 ms (100%), Space: 23.5 MB (30.27%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,33 +1,27 @@
 class MinStack {
 public:
-    stack<int>st1,st2;
-    int mini = INT_MAX;
+    stack<int>st;
+    stack<int>monost;
     MinStack() {
         
     }
     
     void push(int val) {
-        st1.push(val);
-        mini = min(mini, val);
-        if(st2.size()==0 or st2.top()>=val) st2.push(val);
+        st.push(val);
+        if(monost.size()==0 or monost.top()>=val) monost.push(val);
     }
     
     void pop() {
-        int t = st1.top();
-        st1.pop();
-        if(st2.top()==t){
-            st2.pop();
-            if(st2.size()==0) mini = INT_MAX;
-            else mini = st2.top();
-        }
+        int val = st.top(); st.pop();
+        if(monost.top()==val) monost.pop();
     }
     
     int top() {
-        return st1.top();
+        return st.top();
     }
     
     int getMin() {
-        return st2.top();
+        return monost.top();
     }
 };
 
